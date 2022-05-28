d69f19234672112d6faf745d59b1684831cd58d9
JHessler <jhessler11@gmail.com>
2022-05-28 10:03:21 -0400

    profile and thoughts pages

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries'
+import { QUERY_THOUGHT } from '../utils/queries'
 import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
@@ -9,7 +9,7 @@ const SingleThought = props => {
   const { id: thoughtId } = useParams();
   console.log(thoughtId);
 
-  const { loading, data } = useQuery(QUERY_THOUGHTS, {
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
     variables: { id: thoughtId }
   });
   console.log("data", thoughtId);
