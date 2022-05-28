c616ae01dafb71d69701cea21e795cc349c11e65
JHessler <jhessler11@gmail.com>
2022-05-28 09:46:21 -0400

    single thought

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,15 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login'
+import NoMatch from './pages/NoMatch'
+import SingleThought from './pages/SingleThought'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
 
 import Home from './pages/Home';
+import { from } from 'rxjs';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -18,13 +25,44 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile/:username"
+                element={<Profile />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
+              <Route
+                path="/thought/:id"
+                element={<SingleThought />}
+              />
+              <Route
+                path="*"
+                element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
